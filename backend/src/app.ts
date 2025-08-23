1d9fdd1560a49605923b4d8eed0582b57f174058
ThiagoDanielPerez <thiagodanielperez@alu.frlp.utn.edu.ar>
2025-08-23 15:06:34 -0300

    CRUD Deporte y Reseña

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,18 @@
+// backend/src/index.ts
 import express from 'express';
+import cors from 'cors';
+import deportesRouter from './routes/deportes.routes';
+import resenasRouter from './routes/resenas.routes';
+
 const app = express();
-const PORT = 3000;
-// Endpoint Hola Mundo
-app.get('/', (req, res) => {
-    res.send(`👋 ¡Hola mundo desde Express!` );
-});
-// Inicio del servidor
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/deportes', deportesRouter);
+app.use('/api/resenas', resenasRouter);
+
 app.listen(PORT, () => {
-console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
