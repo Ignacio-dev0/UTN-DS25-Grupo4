a069759f84ed9bfca65804d7b8f2e39c3b116c66
Ignacio-dev0 <nacho123benitez@gmail.com>
2025-08-24 18:52:51 -0300

    service,controller,routes de cancha

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import deportesRouter from './routes/deportes.routes';
 import resenasRouter from './routes/resenas.routes';
 import usuarioRoutes from "./routes/usuario.routes";
 import complejoRoutes from './routes/complejo.routes';
+import canchaRoutes from './routes/cancha.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use('/api/deportes', deportesRouter);
 app.use('/api/resenas', resenasRouter);
 app.use("/usuarios", usuarioRoutes);
 app.use('/api/complejos', complejoRoutes); 
+app.use('/api/canchas', canchaRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
