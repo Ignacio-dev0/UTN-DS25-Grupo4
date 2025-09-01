d48565e5aa2a584a5a1fd2a5cf440a839fe2bc04
ariel12C <arielcayo526@gmail.com>
2025-09-01 17:33:17 -0300

    refactoring

--- backend/src/services/horario.service.ts
+++ backend/src/services/horario.service.ts
@@ -2,7 +2,7 @@ import prisma from "../config/prisma";
 import * as horarioInterface from "../types/horarioCronograma.types"
 import { HorarioCronograma } from "../generated/prisma";
 import { primitiveTypes } from "zod/v4/core/util.cjs";
-import { date } from "zod";
+import { date, number } from "zod";
 
 export const createHorario = async(data: horarioInterface.CreateHoraio) => {
     const [hour, minute] = data.hora.split(':').map(Number);
@@ -11,7 +11,7 @@ export const createHorario = async(data: horarioInterface.CreateHoraio) => {
     return prisma.horarioCronograma.create({
         data: {
             hora: horaDate,
-            precio
+            precio: 
         }
     });
 }
