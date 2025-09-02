8228edbb6728fbe069318b019729bc7f37bab30a
MauroAntar <antarmauro@gmail.com>
2025-09-02 16:17:41 -0300

    solicitud service add

--- backend/src/services/solicitud.service.ts
+++ backend/src/services/solicitud.service.ts
@@ -1,77 +1,70 @@
-// import prisma from '../lib/prisma';
-// import { Prisma } from '../generated/prisma/client';
-// import { EstadoSolicitud } from '../generated/prisma/client';
-// import { crearComplejo } from './complejo.service';
+import prisma from '../config/prisma';
+import { EstadoSolicitud } from '../generated/prisma';
+import { crearComplejo } from './complejo.service';
+import { CreateSolicitudRequest, UpdateSolicitudRequest } from '../types/solicitud.types';
 
-// export const crearSolicitud = (data: Prisma.SolicitudCreateInput) => {
-// 	return prisma.solicitud.create({
-// 		data
-// 	});
-// }
+export async function crearSolicitud(data: CreateSolicitudRequest) {
+	return prisma.solicitud.create({ data });
+}
 
-// export const obtenerSolicitudes = () => {
-//   return prisma.solicitud.findMany({
-// 		include: {
-// 			emisor: true,
-// 			documentos: true
-// 		}
-// 	});
-// };
+export async function obtenerSolicitudes() {
+  return prisma.solicitud.findMany({
+		include: { emisor: true, documentos: true }
+	});
+};
 
-// export const obtenerSolicitudesPendientes = () => {
-//   return prisma.solicitud.findMany({
-// 		where: {
-// 			estado: EstadoSolicitud.PENDIENTE
-// 		},
-// 		include: {
-// 			emisor: true,
-// 			documentos: true
-// 		}
-// 	});
-// };
+export async function obtenerSolicitudesPendientes() {
+  return prisma.solicitud.findMany({
+		where: { estado: EstadoSolicitud.PENDIENTE },
+		include: { emisor: true, documentos: true }
+	});
+};
 
-// export const obtenerSolicitudPorId = (id: number) => {
-//   return prisma.solicitud.findUnique({
-//     where: { id }
-//   });
-// };
+export async function obtenerSolicitudPorId(id: number) {
+  const solicitud = prisma.solicitud.findUnique({
+    where: { id }
+  });
 
-// export const evaluarSolicitud = async (
-// 	id: number,
-// 	data: Prisma.SolicitudUpdateInput,
-// 	evaluadorId: number
-// ) => {
+	if (!solicitud) {
+    const error = new Error('Solicitud not Found');
+    (error as any).statusCode = 404;
+    throw error;
+  }
+	return solicitud;
+};
 
-// 	const solicitud = await prisma.solicitud.findUnique({
-// 		where: { id }
-// 	});
+export async function evaluarSolicitud(
+	id: number,
+	data: UpdateSolicitudRequest
+) {
+	const solicitud = await prisma.solicitud.findUnique({
+		where: { id }
+	});
 
-// 	if (solicitud === null) {
-// 		throw(`La solicitud de id: ${id} no existe`);
-// 	}
+	if (!solicitud) {
+    const error = new Error('Solicitud not Found');
+    (error as any).statusCode = 404;
+    throw error;
+  }
 
-// 	if (solicitud.estado !== EstadoSolicitud.PENDIENTE) {
-// 		throw('La solicitud ya fue evaluada');
-// 	}
+	if (solicitud.estado !== EstadoSolicitud.PENDIENTE) {
+		const error = new Error('La solicitud ya fue evaluada');
+    (error as any).statusCode = 404;
+    throw error;
+	}
 
-// 	if (data.estado === EstadoSolicitud.APROBADA) {
-// 		crearComplejo({
-// 			solicitud: {
-// 				connect: { id }
-// 			},
-// 			duenios: {
-// 				connect: { id: solicitud.emisorId }
-// 			},
-// 		});
-// 	}
+	if (data.estado === EstadoSolicitud.APROBADA) {
+		crearComplejo({
+			solicitud: { connect: { id } },
+			duenios: { connect: { id: solicitud.emisorId } },
+		});
+	}
 
-// 	return prisma.solicitud.update({
-// 		where: { id },
-// 		data: {
-// 			estado: data.estado,
-// 			evaluador: {
-// 				connect: { id: evaluadorId }
-// 			},
-// 		},
-// 	});
-// }
+	return prisma.solicitud.update({
+		where: { id },
+		data: {
+			estado: data.estado,
+			evaluador: { connect: { id: data.evaluador.id } },
+		},
+	});
+}
